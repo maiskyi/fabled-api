1df761650be33f58835666f5c2142c0d5252d670
Andrii Maiskyi <amaiskyi@bellum.ai>
2024-11-15 12:24:57 +0000

    feat: api. #1

--- src/_common/dto/bootstrap.dto.ts
+++ src/_common/dto/bootstrap.dto.ts
@@ -2,12 +2,18 @@ import { ApiProperty } from '@nestjs/swagger';
 
 import { MoralLessonsItem } from './moralLessonIte.dto';
 import { Config } from './config.dto';
+import { PromptItem } from './promptItem.dto';
 
 export class Bootstrap {
   @ApiProperty({
     type: [MoralLessonsItem],
   })
   moralLessons: MoralLessonsItem[];
+
+  @ApiProperty({
+    type: [PromptItem],
+  })
+  prompts: PromptItem[];
 
   @ApiProperty({
     type: Config,
