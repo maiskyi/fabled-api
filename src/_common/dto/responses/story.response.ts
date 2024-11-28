ccd64354f49b23e35776a15fdef9474971441015
Andrii Maiskyi <amaiskyi@bellum.ai>
2024-11-28 23:33:52 +0000

    feat: get story. #10

--- src/_common/dto/responses/story.response.ts
+++ src/_common/dto/responses/story.response.ts
@@ -1,10 +1,75 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { StoryStatusType } from '@prisma/client';
+
+export enum StoryStatusLog {
+  ContentInProgress = 'contentInProgress',
+  CreateStoryRequestFailed = 'createStoryRequestFailed',
+  ImageInProgress = 'imageInProgress',
+  Initialized = 'initialized',
+  StoryContentGenerationFailed = 'storyContentGenerationFailed',
+  StoryContentGenerationFailedWithNoResult = 'storyContentGenerationFailedWithNoResult',
+  StoryGenerationFailed = 'storyGenerationFailed',
+  StoryImageGenerationFailed = 'storyImageGenerationFailed',
+  StoryImageGenerationFailedWithNoResult = 'storyImageGenerationFailedWithNoResult',
+  StoryImageUploadFailed = 'storyImageUploadFailed',
+  StoryImageUploadingToCloudinaryFailed = 'storyImageUploadingToCloudinaryFailed',
+  Success = 'success',
+}
+
+class StoryCharacter {
+  @ApiProperty({
+    type: String,
+  })
+  public readonly id: string;
+}
+
+class StoryMoralLesson {
+  @ApiProperty({
+    type: String,
+  })
+  public readonly id: string;
+}
+
+class StoryPlaceOfEvent {
+  @ApiProperty({
+    type: String,
+  })
+  public readonly id: string;
+}
+
+class StoryPrompt {
+  @ApiProperty({
+    type: String,
+  })
+  public readonly id: string;
+}
 
 export class Story {
   @ApiProperty({
     type: String,
   })
   public readonly title: string;
+
+  @ApiProperty({
+    type: String,
+  })
+  public readonly message: string;
+
+  @ApiProperty({
+    enum: StoryStatusType,
+  })
+  public readonly status: StoryStatusType;
+
+  @ApiProperty({
+    enum: StoryStatusLog,
+    isArray: true,
+  })
+  public readonly statusLog: StoryStatusLog[];
+
+  @ApiProperty({
+    type: Date,
+  })
+  public readonly createdAt: Date;
 
   @ApiProperty({
     type: String,
@@ -23,6 +88,27 @@ export class Story {
 
   @ApiProperty({
     type: String,
+    required: false,
   })
   public readonly image: string;
+
+  @ApiProperty({
+    type: StoryCharacter,
+  })
+  public readonly character: StoryCharacter;
+
+  @ApiProperty({
+    type: StoryMoralLesson,
+  })
+  public readonly moralLesson: StoryMoralLesson;
+
+  @ApiProperty({
+    type: StoryPlaceOfEvent,
+  })
+  public readonly placeOfEvent: StoryPlaceOfEvent;
+
+  @ApiProperty({
+    type: StoryPrompt,
+  })
+  public readonly prompt: StoryPrompt;
 }
