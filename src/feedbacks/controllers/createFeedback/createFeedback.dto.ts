8360756b98296af8b90a19baca82cc2d996a17e7
Andrii Maiskyi <andrey@wearehelful.com>
2025-01-12 18:17:29 +0000

    feat: feedback. #126

--- src/feedbacks/controllers/createFeedback/createFeedback.dto.ts
+++ src/feedbacks/controllers/createFeedback/createFeedback.dto.ts
@@ -1,20 +1,35 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, Max, Min } from 'class-validator';
 
 export class CreateFeedbackRequest {
   @ApiProperty({
     type: String,
   })
+  @IsNotEmpty()
   public readonly comment: string;
 
   @ApiProperty({
     type: Number,
+    minimum: 1,
+    maximum: 5,
   })
+  @Min(1)
+  @Max(5)
+  @IsNotEmpty()
   public readonly rating: number;
+
+  @ApiProperty({
+    type: String,
+    required: false,
+  })
+  @IsEmail()
+  public readonly email: string;
 }
 
 export class CreateFeedbackResponse {
   @ApiProperty({
     type: String,
   })
+  @IsNotEmpty()
   public readonly id: string;
 }
